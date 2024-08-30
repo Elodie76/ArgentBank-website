69535b278976f965794d8758c62363e67d816fcd
Elodie <elodie.benard76@gmail.com>
2024-08-30 14:38:30 +0200

    ajout des components footer et hero

--- front/src/pages/signin/Signin.jsx
+++ front/src/pages/signin/Signin.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from '../../components/navigation/Navigation';
+import Footer from '../../components/footer/Footer';
 
 const Signin = () => {
     return (
         <div>
             <Navigation />
             <h1>Sign-in</h1>
+            <Footer />
         </div>
     );
 };
--- front/src/pages/user/User.jsx
+++ front/src/pages/user/User.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Navigation from '../../components/navigation/Navigation';
+import Footer from '../../components/footer/Footer';
 
 const User = () => {
     return (
         <div>
-            <h1>User</h1>
+            <Navigation />
+            <main>
+                <h1>User</h1>    
+            </main>
+            <Footer />
         </div>
     );
 };
